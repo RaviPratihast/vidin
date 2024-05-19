b8a28e59e2c932a720936ab8e32d75cbbfb84e6c
Ravi Pratihast <ravipratihast@gmail.com>
2024-05-19 14:38:09 +0530

    add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import "./App.css";
+import React from "react";
 
 function App() {
-  return <div className="App">VidIn</div>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
