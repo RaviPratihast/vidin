d9db93f80fd7dd449144fbee1e1f89857ae5d531
Ravi Pratihast <ravipratihast@gmail.com>
2024-05-20 20:56:55 +0530

    add: component mounted.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Header } from "./component/index-component";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="App">
+      <Header />
+      {/* Main content will go here */}
+      <div className="p-4">
+        <h1 className="text-4xl font-bold text-center mt-20">
+          Welcome to vidIn
+        </h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
