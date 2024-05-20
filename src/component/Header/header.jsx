063df13652d7a556c3be95b80ab74e5704e0e321
Ravi Pratihast <ravipratihast@gmail.com>
2024-05-20 20:55:27 +0530

    add: header component added

---
+++ src/component/Header/header.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { Button } from "../index-component";
+
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <header className="bg-gray-800 text-white p-4 flex items-center justify-between">
+      <div className="flex items-center">
+        <Button
+          className="mr-4 md:hidden"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
+          </svg>
+        </Button>
+
+        <span className="text-2xl font-bold">vidIn</span>
+        <div className="hidden lg:flex ml-11 mt-1">
+          <ul className="flex gap-6">
+            <li className="text-lg cursor-pointer">Watch Later</li>
+            <li className="text-lg cursor-pointer">Liked</li>
+            <li className="text-lg cursor-pointer">History</li>
+            <li className="text-lg cursor-pointer">Playlist</li>
+          </ul>
+        </div>
+      </div>
+      <div className="hidden md:flex  items-center justify-center">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="p-2 rounded bg-gray-700 text-white w-96  focus:outline-none focus:ring-2 focus:ring-gray-500"
+        />
+      </div>
+
+      <div className="flex items-center">
+        <Button className="bg-gray-700 text-white py-2 px-4 rounded border border-transparent hover:bg-gray-600 hover:border-white">
+          Login
+        </Button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
