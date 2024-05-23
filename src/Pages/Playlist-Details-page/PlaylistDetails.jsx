b725946b5afd2ec50d977057e7fa9656bc61c92a
Ravi Pratihast <ravipratihast@gmail.com>
2024-05-23 12:06:04 +0530

    add: pages are created.

---
+++ src/Pages/Playlist-Details-page/PlaylistDetails.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PlaylistDetails = () => {
+  return <div>PlaylistDetails</div>;
+};
+
+export { PlaylistDetails };
---
+++ src/Pages/index-page.js
@@ -0,0 +1,21 @@
+import { Explore } from "./Explore-page/Explore";
+import { History } from "./History-page/History";
+import { Liked } from "./Liked-page/Liked";
+import { Login } from "./Login-page/Login";
+import { PlaylistDetails } from "./Playlist-Details-page/PlaylistDetails";
+import { PlaylistPage } from "./Playlist-page/PlaylistPage";
+import { SignUp } from "./SignUp-page/SignUp.jsx";
+import { VideoDetails } from "./Video-Details-page/VideoDetails.jsx";
+import { WatchLater } from "./Watch-Later-page/WatchLater.jsx";
+
+export {
+  Explore,
+  History,
+  Liked,
+  Login,
+  PlaylistDetails,
+  PlaylistPage,
+  SignUp,
+  VideoDetails,
+  WatchLater,
+};
