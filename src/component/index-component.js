c2c52cdf1cd99143a9891e96668b25475b201a4d
Ravi Pratihast <ravipratihast@gmail.com>
2024-05-23 12:58:52 +0530

    add: video card added.

--- src/component/index-component.js
+++ src/component/index-component.js
@@ -1,4 +1,5 @@
 import Header from "./Header/header";
 import { Button } from "./Button/Button";
+import { VideoCard } from "./Card/VideoCard";
 
-export { Header,Button };
+export { Header, Button, VideoCard };
