3806d70c84b37c45c14a7a1169f04909433d6967
Ravi Pratihast <ravipratihast@gmail.com>
2024-05-27 16:50:56 +0530

    style: related to screen size

--- src/Pages/Explore-page/Explore.jsx
+++ src/Pages/Explore-page/Explore.jsx
@@ -4,8 +4,9 @@ import { VideoCard } from "../../component/index-component";
 
 const Explore = () => {
   const { state } = useVideo();
+
   return (
-    <div className="min-h-screen p-6">
+    <div className="min-h-screen pt-16 p-6 bg-gray-100">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
         {state.initialVideo.map((video) => (
           <VideoCard key={video.id} video={video} />
