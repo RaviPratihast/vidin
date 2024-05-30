b4b85270706e686eb5f3bf993696676c04e575b4
Ravi Pratihast <ravipratihast@gmail.com>
2024-05-31 03:51:33 +0530

    Add: Playlist_Details Mounted.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import { Header } from "./component/index-component";
 import {
   Liked,
   WatchLater,
   History,
-  PlaylistPage,
+  Playlist,
   Explore,
   VideoDetails,
+  PlaylistDetail,
 } from "./Pages/index-page";
 
 function App() {
@@ -26,8 +27,12 @@ function App() {
           <Route path="/watchLater" element={<WatchLater />} />
           <Route path="/liked" element={<Liked />} />
           <Route path="/history" element={<History />} />
-          <Route path="/playlistPage" element={<PlaylistPage />} />
+          <Route path="/playlistPage" element={<Playlist />} />
           <Route path="/video/:videoId" element={<VideoDetails />} />
+          <Route
+            path="/playlistDetail/:playlistId"
+            element={<PlaylistDetail />}
+          />
         </Routes>
         <ToastContainer autoClose={700} />
       </div>
